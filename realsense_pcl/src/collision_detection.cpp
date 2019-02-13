741ed8f33c1c417451d308b1a53533247140345a
Hayden Nichols <hln0001@mix.wvu.edu>
2019-02-13 17:24:37 -0500

    half-functional

--- realsense_pcl/src/collision_detection.cpp
+++ realsense_pcl/src/collision_detection.cpp
@@ -10,7 +10,7 @@
 
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/passthrough.h>
-#include <messages/CollisionTrajectory.h>
+#include <pcl_messages/CollisionTrajectory.h>
 #include <math.h>
 
 double camera_height; //camera_height off the ground for ground removal
@@ -39,8 +39,9 @@ int threshold_min_angle;
 int collision_threshold; //number of points in collision to trigger avoid/slowdown
 
 ros::Publisher pub;
+ros::Publisher pub2;
 
-messages::CollisionTrajectory traj_msg;
+pcl_messages::CollisionTrajectory traj_msg;
 
 int collisionCounter(double angle, double distance)
 {
@@ -196,43 +197,51 @@ void generateHazardMap(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud)
 void corridorCallback(const sensor_msgs::PointCloud2& cloud_msg)
 {
   //make container for converted cloud
-  pcl::PCLPointCloud2 cloud_temp;
+  pcl::PCLPointCloud2::Ptr cloud_temp (new pcl::PCLPointCloud2 ());
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
 
   //convert cloud to pcl
-  pcl_conversions::toPCL(cloud_msg, cloud_temp);
-  pcl::fromPCLPointCloud2(cloud_temp, *cloud);
+  pcl_conversions::toPCL(cloud_msg, *cloud_temp);
+  pcl::fromPCLPointCloud2(*cloud_temp, *cloud);
 
   //Passthrough filter the points in z
   pcl::PassThrough<pcl::PointXYZRGB> pass;
+
   pass.setInputCloud(cloud);
   pass.setFilterFieldName("z");
-  pass.setFilterLimits(- reactive_height , camera_height * 1.1); //change these
+  pass.setFilterLimits(-reactive_height , camera_height * 1.1);
   pass.filter(*cloud);
 
   //Passthough in y
   pass.setInputCloud(cloud);
   pass.setFilterFieldName("y");
-  pass.setFilterLimits(- hazard_map_size_y,  hazard_map_size_y); //not sure what these should be
+  pass.setFilterLimits(-hazard_map_size_y,  hazard_map_size_y);
+  //pass.setFilterLimits(-10,  10);
   pass.filter(*cloud);
 
   //passthrough filter the points in x
   pass.setInputCloud(cloud);
   pass.setFilterFieldName("x");
-  pass.setFilterLimits(- hazard_map_size_x_neg, hazard_map_size_x_pos); //again, not sure what to choose
+  pass.setFilterLimits(-hazard_map_size_x_neg, hazard_map_size_x_pos); 
   pass.filter(*cloud);
 
   //Voxel Grid Filter the points
   pcl::VoxelGrid<pcl::PointXYZRGB> vox;
   vox.setInputCloud(cloud);
-  vox.setLeafSize(0.01,0.01,0.01);
-  vox.filter(*cloud); //I should make it so I don't have to pass these as arguments.
+  vox.setLeafSize(0.02,0.02,0.02);
+  vox.filter(*cloud); 
+
+  int cloudsize = cloud->points.size();
+  ROS_INFO("%i",cloudsize);
+
+  pub2.publish(cloud);
 
   collision_left = 0;
   collision_right = 0;
   collision_avoid = 0;
   collision_slowdown = 0;
   //collision_status = 0;
+
 
   for(int i = 0; i < cloud->points.size(); i++)
   {
@@ -259,7 +268,6 @@ void corridorCallback(const sensor_msgs::PointCloud2& cloud_msg)
         }
       }
     }
-
   if (collision_slowdown > collision_threshold)
   {
     traj_msg.slowdown = true;
@@ -319,17 +327,17 @@ void corridorCallback(const sensor_msgs::PointCloud2& cloud_msg)
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "planar_segmentation");
+  ros::init(argc, argv, "collision_detection");
   ros::NodeHandle nh;
 
-  double hazard_map_size_y;
-  double hazard_map_size_x_neg;
-  double hazard_map_size_x_pos;
+  //double hazard_map_size_y;
+  //double hazard_map_size_x_neg;
+  //double hazard_map_size_x_pos;
 
-  int collision_slowdown; //counter for points within the slowdown region
-  int collision_avoid; //counter for points within avoid region
-  int collision_right; //counter for points to the left
-  int collision_left; //counter for points to the left
+  //int collision_slowdown; //counter for points within the slowdown region
+  //int collision_avoid; //counter for points within avoid region
+  //int collision_right; //counter for points to the left
+  //int collision_left; //counter for points to the left
 
   nh.param("collision_threshold", collision_threshold, 100); //number of points in collision to trigger avoid/slowdown
   nh.param("camera_height", camera_height, 0.2);
@@ -343,10 +351,13 @@ int main(int argc, char** argv)
   nh.param("threshold_obstacle_number", threshold_obstacle_number, 100);
   nh.param("near_dist", near_dist, 4.0);
   nh.param("far_dist", far_dist, 8.0);
+  nh.param("hazard_map_size_x_neg", hazard_map_size_x_neg, 1.0);
+  nh.param("hazard_map_size_x_pos", hazard_map_size_x_pos, 10.0);
 
-  ros::Subscriber sub = nh.subscribe("rover_cloud", 1, corridorCallback);
+  ros::Subscriber sub = nh.subscribe("camera/depth_registered/points", 1, corridorCallback);
 
-  pub = nh.advertise<messages::CollisionTrajectory>("collision_trajectory", 1);
+  pub = nh.advertise<pcl_messages::CollisionTrajectory>("collision_trajectory", 1);
+  pub2 = nh.advertise<sensor_msgs::PointCloud2>("filtered",1);
 
   ros::spin();
 }
